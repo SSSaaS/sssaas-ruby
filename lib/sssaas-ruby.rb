1a538dfba45b1b6efcbacf7c8f2623410bcf922f
Alexander Scheel <alexander.m.scheel@gmail.com>
2016-01-03 22:26:24 -0600

    Initial from_yaml, from_config

--- lib/sssaas-ruby.rb
+++ lib/sssaas-ruby.rb
@@ -2,25 +2,61 @@ require 'sssa'
 require 'net/http'
 require 'uri'
 require 'json'
+require 'yaml'
 
 module SSSaaS
     module API
-        def self.get_secret(serveruris, tokens, secrets, timeout=300)
-            results = secrets
+        def self.from_yaml(data, key)
+            obj = YAML.load(data)
 
-            unless serveruris.class == [].class
-                serveruris = [serveruris]
+            return self.from_config(obj[key])
+        end
+
+        def self.from_config(obj)
+            if obj.nil?
+                return ""
             end
 
-            unless tokens.class == [].class
-                tokens = [tokens]
+            if obj["remote"].nil?
+                raise :invalid_config, "Invalid config object -- missing remote"
             end
 
-            threads = []
+            if obj["shares"].nil? or obj["local"].nil? or
+                raise :invalid_config, "Invalid config object -- missing shares or local"
+            end
 
-            serveruris.each_with_index do |serveruri, index|
-                threads.push(Thread.new do
-                    uri = URI.parse(serveruri + '?key=' + tokens[index])
+            if obj["minimum"].nil?
+                raise :invalid_config, "Invalid config object -- missing minimum"
+            end
+
+            return self.get_secret()
+        end
+
+        def self.get_secret(endpoints, shares, minimum, timeout=300)
+            results = shares
+
+            if endpoints.class == "string".class
+                endpoints = [endpoints]
+            elsif endpoints.class != [].class
+                raise :invalid_config, "Invalid config object -- remote should be an array of URIs"
+            end
+
+            if shares.class == "string".class
+                shares = [shares]
+            elsif shares.class != [].class
+                raise :invalid_config, "Invalid config object -- shares should be an array of shares"
+            end
+
+            unless minimum.class = 1234567890.class
+                raise :invalid_config, "Invalid config object -- shares should be an array of shares"
+            end
+
+            tpool = []
+            errors = []
+
+            endpoints.each_with_index do |serveruri, index|
+                tpool.push(Thread.new do
+                    uri = URI.parse(serveruri)
 
                     Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
                         http.read_timeout = timeout
@@ -28,18 +64,25 @@ module SSSaaS
                         request.add_field('User-Agent', 'sssaas-ruby v0 v0.0.2')
                         response = http.request request
                         unless response.kind_of? Net::HTTPSuccess
-                            raise response.class.to_s + ': ' + response.message
+                            errors.push response.class.to_s + ': ' + response.message
                         else
-                            (JSON.parse(response.body)['sharedSecrets']).each {|s|
+                            (
+                                JSON.parse(response.body)['sharedSecrets']
+                            ).each { |s|
                                 s.strip!
                                 results.push s
                             }
+                            results = results.uniq
+
+                            if results.size > minimum
+                                tpool.each { |t| t.kill }
+                            end
                         end
                     end
                 end)
             end
 
-            threads.each { |t|  t.join }
+            tpool.each { |t|  t.join }
 
             results = results.uniq
 
--- tests/sssaas-ruby.rb
+++ tests/sssaas-ruby.rb
@@ -3,6 +3,10 @@ require 'test/unit'
 
 class TestSSSaaS < Test::Unit::TestCase
     def test_api()
-        assert(SSSaaS::API.get_secret(["https://sssaas.cipherboy.com:8444/api/v0/request/test"], ["sssaas-library-test-allowed"], ["j8-Y4_7CJvL8aHxc8WMMhP_K2TEsOkxIHb7hBcwIBOo=T5-EOvAlzGMogdPawv3oK88rrygYFza3KSki2q8WEgs=", "wGXxa_7FPFSVqdo26VKdgFxqVVWXNfwSDQyFmCh2e5w=8bTrIEs0e5FeiaXcIBaGwtGFxeyNtCG4R883tS3MsZ0="]) == "test-pass", "Error with API testing...")
+        api = "https://sssaas.cipherboy.com:8444/api/v0/request/test?key=sssaas-library-test-allowed"
+        shares = ["j8-Y4_7CJvL8aHxc8WMMhP_K2TEsOkxIHb7hBcwIBOo=T5-EOvAlzGMogdPawv3oK88rrygYFza3KSki2q8WEgs=", "wGXxa_7FPFSVqdo26VKdgFxqVVWXNfwSDQyFmCh2e5w=8bTrIEs0e5FeiaXcIBaGwtGFxeyNtCG4R883tS3MsZ0="]
+        minimum = 4
+        secret = SSSaaS::API.get_secret(api, shares, minimum)
+        assert(secret == "test-pass", "Error with API testing...")
     end
 end
